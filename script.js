5a93b3111176719a5ce4a834eae82fca4271e356
colyn <colyn.gremaud@gmail.com>
2022-11-18 16:52:31 -0600

    forgot to save

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.addEventListener("load", () => {
     let greeterButton = document.getElementById("greeterButton")  
 
     greeterButton.addEventListener("click", () => {
-        let name = "colyn";
         let greeting = document.getElementById("greeting")
         let input = document.getElementById("name").value;
         greeting.innerHTML =  `Hello, ${input}`;
